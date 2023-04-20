18fb5468cdd489bdbedfea8b7dc1fcfbe3eac0ae
Washington <wlanjos@hotmail.com>
2023-04-20 15:18:27 -0300

    first commit

---
+++ src/main/java/br/com/ekan/plano/PlanoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ekan.plano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ekan/plano/PlanoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ekan.plano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
